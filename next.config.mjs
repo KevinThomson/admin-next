497adf6bd01a4cd6231a04604516d7df90f53d07
Kevin Thomson <kev.thomson@gmail.com>
2024-11-24 20:44:20 -0600

    chore: add webpack HMR config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // Enable webpack HMR
+    webpack: (config) => {
+      config.experiments = {
+        ...config.experiments,
+        topLevelAwait: true,
+      }
+      return config
+    },
+  }
 
 export default nextConfig;
